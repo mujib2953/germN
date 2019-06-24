91462768457c9e885aafef9517b5bc3599a3e0fe
rehmanm9c <mujibur.rehman@citiustech.com>
2019-06-24 18:10:22 +0530

    dot env package and environment created.

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
+import dotenv from "dotenv";
 import express from "express";
 
+// initialize configuration
+dotenv.config();
+
 const app: express.Application = express();
-const PORT: number = parseInt(process.env.PORT, 10) || 5000;
+const PORT: number = parseInt(process.env.SERVER_PORT, 10) || 5000;
 
 app.get("/", (req, res) => {
     res.send("Happy coding...");
